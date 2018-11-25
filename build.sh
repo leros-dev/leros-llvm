0160e0d7b51ecda9d583d86242aa42c50c19e1c7
Morten Borup Petersen <morten_bp@live.dk>
2018-11-25 21:35:13 +0100

    Make build script more robust

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
+CLANG_VERSION="8.0.0"
 BUILD_DIRECTORY=build-leros-llvm
 
-# Make sure current working directory is the directory of the build script
+# Make sure current working directory is leros-llvm
 cd "${0%/*}"
 
 SOURCE_ROOT=$(pwd)
-
-CLANG_VERSION="8.0.0"
+PARENT_DIR="$(dirname "${SOURCE_ROOT}")"
 
 # Binary folder of the built compiler
-BUILD_ROOT="$SOURCE_ROOT/../$BUILD_DIRECTORY"
+BUILD_ROOT="$PARENT_DIR/$BUILD_DIRECTORY"
 BUILD_BIN_DIR="$BUILD_ROOT/bin"
 BUILD_CLANG_LIB_DIR="$BUILD_ROOT/lib/clang/$CLANG_VERSION"
 
 # Create build folder next to source
-mkdir ../$BUILD_DIRECTORY
-cd ../$BUILD_DIRECTORY
+mkdir $BUILD_ROOT
+cd $BUILD_ROOT
 
-# Run CMake configuration
-cmake -DLLVM_TOOL_CLANG_BUILD=OFF -DLLVM_TARGETS_TO_BUILD="" LLVM_DEFAULT_TARGET_TRIPLE="leros32-unknown-elf" -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="Leros" -DCMAKE_BUILD_TYPE="Release" $SOURCE_ROOT
+# Run CMake configuration of leros-llvm inside build directory
+cmake -DLLVM_TOOL_CLANG_BUILD=OFF -DLLVM_TARGETS_TO_BUILD="" -DLLVM_DEFAULT_TARGET_TRIPLE="leros32-unknown-elf" -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="Leros" -DCMAKE_BUILD_TYPE="Release" $SOURCE_ROOT
 
 # Use all cores when building
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
@@ -32,16 +32,19 @@ fi
 cmake --build .
 
 # ============ leros-lib =================
-cd ..
+LEROS_LIB_DIR="$PARENT_DIR/leros-lib"
+LEROS_LIB_DIR="$PARENT_DIR/leros-lib"
+cd "$PARENT_DIR"
+
 # Check if the leros-lib repo is checked out next to the leros-llvm repository
-if [ -d "leros-lib" ]; then
+if [ -d "$LEROS_LIB_DIR" ]; then
   # make sure the repository is up-to-date
-  cd leros-lib
+  cd $LEROS_LIB_DIR
   git pull
 else
   # clone the leros-lib repository
   git clone https://github.com/leros-dev/leros-lib
-  cd leros-lib
+  cd $LEROS_LIB_DIR
 fi
 
 # Build crt0.leros.o
@@ -53,13 +56,8 @@ mv crt0.leros.o "$BUILD_CLANG_LIB_DIR/crt0.leros.o"
 RUNTIME_LIBRARY_NAME="libclang_rt.builtins-leros32.a"
 cd ..
 /bin/bash build_runtime.sh "$BUILD_BIN_DIR"
-cd build-leros-compiler-rt-builtins
-cd lib
-cd linux
+cd "build-leros-compiler-rt-builtins"
+cd "lib"
+cd "linux"
+mkdir "${BUILD_CLANG_LIB_DIR}/lib"
 mv "$RUNTIME_LIBRARY_NAME" "$BUILD_CLANG_LIB_DIR/lib/$RUNTIME_LIBRARY_NAME"
-
-
-# Download crt0.leros.o from leros-lib
-cd bin
-wget "https://github.com/leros-dev/leros-lib/blob/master/runtime/crt0.leros.o"
-
